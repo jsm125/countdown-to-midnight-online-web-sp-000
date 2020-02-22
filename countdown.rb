2e3853c74e9efa906a50d5143e6197ea4ea267eb
Matt Im <matthew.seiim@gmail.com>
2020-02-22 19:51:57 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,12 +3,12 @@
 def countdown(i)
   while i > 0
     puts "#{i} SECOND(S)!"
-
+  countdown_with_sleep
   i -= 1
   end
   return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  sleep(1)
+def countdown_with_sleep(i)
+  sleep(i)
 end
